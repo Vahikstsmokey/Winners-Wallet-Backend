7e3165acfd3ca95b17303554ec9c4f56118ac32d
Gor <gorgevorgyan255581>
2025-09-03 12:39:36 +0400

    start import funcional and add rout

--- src/controllers/wallet.controller.js
+++ src/controllers/wallet.controller.js
@@ -3,29 +3,38 @@ import { ownerWallet } from "../config/web3.config.js";
 import createError from "http-errors";
 
 class WalletController {
-    static async createWallet(req, res, next) {
-        try {
-            const wallet = ethers.Wallet.createRandom();
+  static async createWallet(req, res, next) {
+    try {
+      const wallet = ethers.Wallet.createRandom();
 
-            const fundingTx = await ownerWallet.sendTransaction({
-                to: wallet.address,
-                value: ethers.parseEther("1.0")
-            });
-            await fundingTx.wait();
+      const fundingTx = await ownerWallet.sendTransaction({
+        to: wallet.address,
+        value: ethers.parseEther("1.0"),
+      });
+      await fundingTx.wait();
 
-            res.status(201).json({
-                wallet: {
-                address: wallet.address,
-                privateKey: wallet.privateKey,
-                mnemonic: wallet.mnemonic.phrase
-            }
-            })
-
-        } catch (e) {
-            next(createError(500, "Failed to create wallet"));
-            next();
-        }
+      res.status(201).json({
+        wallet: {
+          address: wallet.address,
+          privateKey: wallet.privateKey,
+          mnemonic: wallet.mnemonic.phrase,
+        },
+      });
+    } catch (e) {
+      next(createError(500, "Failed to create wallet"));
+      next();
     }
+  }
+
+  static async importWallet(req, res, next) {
+    try {
+        const { privateKey } = req.body;
+        
+    } catch (e) {
+      next(createError(500, e));
+      next();
+    }
+  }
 }
 
 export default WalletController;
--- src/routes/wallet.router.js
+++ src/routes/wallet.router.js
@@ -4,5 +4,6 @@ import WalletController from "../controllers/wallet.controller.js";
 const router = Router();
 
 router.post("/create", WalletController.createWallet);
+router.post("/import", WalletController.importWallet);
 
 export default router;
